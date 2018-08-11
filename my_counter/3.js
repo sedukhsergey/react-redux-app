0b25fced9d11cb0740fc6ccc7540203e977bf280
umbrella121 <31348828+umbrella121@users.noreply.github.com>
2018-08-11 17:36:50 +0300

    implemented outh

---
+++ my_counter/3.js
@@ -0,0 +1,4 @@
+function(login, pas) {
+ if (login === 'login' && pas === 'password') 
+	return true
+ else return false
---
+++ my_counter/outh.js
@@ -0,0 +1,7 @@
+function outhenticate(login, password){
+ if(login === 'login' && password === 'password' { 
+   return 'you were login in'}
+  else {
+  return 'You are not login'
+  }
+}
